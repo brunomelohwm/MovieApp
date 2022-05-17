9f78c2c5a04e23949f458511bd5551e4f3673b00
Bruno Melo <brunomelohwm@gmail.com>
2022-05-17 00:09:10 -0300

    correções

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movies_app/app_widget.dart';
 
-import 'package:movies_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MovieApp());
+    await tester.pumpWidget(const MovieApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
